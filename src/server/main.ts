dc7248e4f7e2d65969eba557f3060df26889d40d
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-09-17 17:05:19 -0400

    feat: establish handshake between FE and BE

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,7 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { GraphQLSchemaHost } from '@nestjs/graphql';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { writeFileSync } from 'fs';
 import { printSchema } from 'graphql';
@@ -12,18 +11,10 @@ import { AppModule } from './app.module';
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule, { cors: true });
 
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
   app.use(cookieParser());
-
-  const config = new DocumentBuilder()
-    .setTitle('Praxis')
-    .setDescription('Social networking platform built with Vite and NestJS')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-
-  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.SERVER_PORT);
 
