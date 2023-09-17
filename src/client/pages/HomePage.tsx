42835a79429c8c9e68fe2207dc022092bc5050f8
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-09-17 13:18:36 -0400

    feat: add mui, add theme from praxis-ui

--- src/client/pages/HomePage.tsx
+++ src/client/pages/HomePage.tsx
@@ -1,7 +1,12 @@
+import { Typography } from '@mui/material';
 import React from 'react';
 
 const HomePage = () => {
-  return <>Welcome to Praxis</>;
+  return (
+    <Typography variant="h2" textAlign="center" paddingTop={4}>
+      Welcome to Praxis
+    </Typography>
+  );
 };
 
 export default HomePage;
