7735354e5f3c8e74ea81919895c474d8d6f524f1
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-09-23 10:46:39 -0400

    refactor: temporarily disable EditRoleTabs, remove next import

--- src/client/components/Roles/EditRoleTabs.tsx
+++ src/client/components/Roles/EditRoleTabs.tsx
@@ -1,10 +1,8 @@
 import { Card, Tab, Tabs } from '@mui/material';
-import { useRouter } from 'next/router';
-import { SyntheticEvent, useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { EditGroupRoleTabsFragment } from '../../apollo/groups/generated/EditGroupRoleTabs.fragment';
 import { EditServerRoleTabsFragment } from '../../apollo/roles/generated/EditServerRoleTabs.fragment';
-import { EditRoleTabNames } from '../../constants/role.constants';
 import { useAboveBreakpoint } from '../../hooks/shared.hooks';
 import DeleteGroupRoleButton from '../Groups/GroupRoles/DeleteGroupRoleButton';
 import GroupPermissionsForm from '../Groups/GroupRoles/GroupPermissionsForm';
@@ -19,48 +17,48 @@ interface Props {
 }
 
 const EditRoleTabs = ({ role }: Props) => {
-  const [tab, setTab] = useState(0);
+  const [tab /*, setTab*/] = useState(0);
 
-  const { query, replace } = useRouter();
   const { t } = useTranslation();
   const isAboveSmall = useAboveBreakpoint('sm');
 
   const isGroupRole = 'group' in role;
 
-  useEffect(() => {
-    if (query.tab === EditRoleTabNames.Permissions) {
-      setTab(1);
-      return;
-    }
-    if (query.tab === EditRoleTabNames.Members) {
-      setTab(2);
-      return;
-    }
-    setTab(0);
-  }, [query.tab]);
+  // TODO: Restore functionality to change tabs based on query params
+  // useEffect(() => {
+  //   if (query.tab === EditRoleTabNames.Permissions) {
+  //     setTab(1);
+  //     return;
+  //   }
+  //   if (query.tab === EditRoleTabNames.Members) {
+  //     setTab(2);
+  //     return;
+  //   }
+  //   setTab(0);
+  // }, [query.tab]);
 
-  const handleTabChange = (_: SyntheticEvent<Element, Event>, value: number) => {
-    if (value === 1) {
-      replace({
-        query: { ...query, tab: EditRoleTabNames.Permissions },
-      });
-      return;
-    }
-    if (value === 2) {
-      replace({
-        query: { ...query, tab: EditRoleTabNames.Members },
-      });
-      return;
-    }
-    delete query.tab;
-    replace({ query });
-  };
+  // const handleTabChange = (_: SyntheticEvent<Element, Event>, value: number) => {
+  //   if (value === 1) {
+  //     replace({
+  //       query: { ...query, tab: EditRoleTabNames.Permissions },
+  //     });
+  //     return;
+  //   }
+  //   if (value === 2) {
+  //     replace({
+  //       query: { ...query, tab: EditRoleTabNames.Members },
+  //     });
+  //     return;
+  //   }
+  //   delete query.tab;
+  //   replace({ query });
+  // };
 
   return (
     <>
       <Card sx={{ marginBottom: 6 }}>
         <Tabs
-          onChange={handleTabChange}
+          // onChange={handleTabChange}
           value={tab}
           variant={isAboveSmall ? 'standard' : 'fullWidth'}
           centered
